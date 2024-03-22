3c5835aa28b860f7ccf8d2fa3caf29b850fb67fe
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-03-22 17:27:25 -0400

    fixed layout for editItem page

--- src/components/EditHomeItem/EditHomeItem.jsx
+++ src/components/EditHomeItem/EditHomeItem.jsx
@@ -25,12 +25,17 @@ function EditHomeItem() {
         });
     }
 
+    //onSubmit, this function will handle the evt
     const handleSaveChanges = (evt) => {
         evt.preventDefault();
         console.log(updateItem);
         axios.put(`/api/item/${updateItem.id}`, updateItem) 
         .then(res => {
             dispatch({type: 'CLEAR_UPDATE'})
+            Swal.fire({
+              confirmButtonColor: "#ADD444",
+              title: "Success. Changes have been saved!"
+            });
             history.push('/dashboard'); //redirect back to user dashboard
         })
         .catch(err => {
@@ -38,6 +43,7 @@ function EditHomeItem() {
         })
     }
 
+    //called for onClick evt listener for cancel button
     const handleCancel = () => {
       console.log('cancel button clicked.');
       Swal.fire({
@@ -70,44 +76,62 @@ function EditHomeItem() {
             <Icon size="large" name="angle left" onClick={() => history.push('/dashboard')} /> <span>Back to dashboard</span>
  
             <form onSubmit={handleSaveChanges}>
-            <input
-              type="text"
-              required
-              value={updateItem.name}
-              placeholder="item name"
-              onChange={(evt) => handleChange(evt, 'name')}
-            />
-            <input
-              type="number"
-              required
-              value={updateItem.priority_level}
-              placeholder="priority level"
-              onChange={(evt) => handleChange(evt, 'priority_level')}
-            />
-            <input
-              type="text"
-              required
-              value={updateItem.location}
-              placeholder="location"
-              onChange={(evt) => handleChange(evt, 'location')}
-            />
-            <Datetime
-              type="date"
-              name="re_date"
-              required
-              value={replaceDate}
-              onChange={date => setReplaceDate(date)}
-            />
-            <div className="saveEdit-btn">
-                  <ActionButtons name="Save Changes" /> 
-                  <span className="cancel" onClick={handleCancel}>Cancel</span>
+            <div className="form-wrapper">
+              <div className="edit-input">
+                <label htmlFor="name">Name:</label>
+                <input
+                type="text"
+                required
+                value={updateItem.name}
+                placeholder="item name"
+                onChange={(evt) => handleChange(evt, 'name')}
+                />
+              </div>
+              <div className="edit-input">
+              <label htmlFor="priority">Priority Level:</label>
+                <select value={updateItem.priority_level} required onChange={(evt) => handleChange(evt, 'priority_level')}>
+                  <option value=''>Select</option>
+                  <option value='1'>1</option>
+                  <option value='2'>2</option>
+                  <option value='3'>3</option>
+                </select>
+              </div>
+              <div className="edit-input">
+              <label htmlFor="location">Location:</label>
+              <select value={updateItem.location} required onChange={(evt) => handleChange(evt, 'location')}>
+                <option value=''>Select location</option>
+                  <option value='attic'>attic</option>
+                  <option value='bathroom'>bathroom</option>
+                  <option value='basement'>basement</option>
+                  <option value='bedroom'>bedroom</option>
+                  <option value='dining room'>dining room</option>
+                  <option value='exterior'>exterior</option>
+                  <option value='foyer'>foyer</option>
+                  <option value='garage'>garage</option>
+                  <option value='hallway'>hallway</option>
+                  <option value='kitchen'>kitchen</option>
+                  <option value='outdoors'>outdoors</option>
+              </select>
+              </div>
+              <div className="edit-input">
+              <Datetime
+                type="date"
+                name="re_date"
+                required
+                value={replaceDate}
+                onChange={date => setReplaceDate(date)}
+              />
+              </div>
             </div>
+              <div className="saveEdit-btn">
+                    <ActionButtons name="Save Changes" /> 
+                    <span className="cancel" onClick={handleCancel}>Cancel</span>
+              </div>
             </form>
           </div>
         </div>
       </div>
-      </>
-      );
+      </>);
 }
 
 export default EditHomeItem
